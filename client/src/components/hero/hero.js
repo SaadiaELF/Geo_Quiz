46bd4f89812e746a20d68c3cbe9cf3499b46754f
SaadiaELF <74792703+SaadiaELF@users.noreply.github.com>
2021-12-29 14:37:46 +0000

    Adding hero

---
+++ client/src/components/hero/hero.js
@@ -0,0 +1,16 @@
+import { Container, Row } from "react-bootstrap";
+import "./hero.css";
+
+function Hero() {
+    return (
+        <div>
+            <Container className="hero">
+                <Row>
+    
+                </Row>
+            </Container>
+        </div>
+    )
+}
+
+export default Hero;

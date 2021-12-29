436a29ccf577dca68dd6df00e2ec320e58291157
SaadiaELF <74792703+SaadiaELF@users.noreply.github.com>
2021-12-29 15:29:39 +0000

    Adding Home content

--- client/src/components/hero/hero.js
+++ client/src/components/hero/hero.js
@@ -1,4 +1,4 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import "./hero.css";
 
 function Hero() {
@@ -6,9 +6,14 @@ function Hero() {
         <div>
             <Container className="hero">
                 <Row>
-                    <Col>
+                    <Col md='12' lg="6" xl="5" className="offset-xl-1 text-center mt-5">
+                        <h1>GeoQuiz</h1>
+                        <p>Aute Lorem non occaecat ullamco culpa officia. Adipisicing do do pariatur id. Reprehenderit magna elit eu do laboris pariatur adipisicing dolor eu Lorem sint enim excepteur aliquip. Tempor nostrud esse enim fugiat. Labore reprehenderit dolor cillum ipsum culpa officia nisi amet id dolor veniam dolore incididunt qui. Voluptate consectetur labore ipsum dolor aliquip voluptate sit. </p>
+                        <Button variant="outline-light" size="lg" >
+                            Start
+                        </Button>
                     </Col>
-                    <Col>
+                    <Col md="5" lg="5" className="offset-lg-1 offset-xl-0 d-none d-lg-block">
                     </Col>
                     {/* <div class="col-12 col-lg-6 col-xl-5 offset-xl-1">
                         <h1>The revolution is here.</h1>

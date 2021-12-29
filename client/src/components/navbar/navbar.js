ae694337f9568b3d6ca349af411d58d3a4904e53
SaadiaELF <74792703+SaadiaELF@users.noreply.github.com>
2021-12-29 03:10:59 +0000

    Adding navbar separate component

---
+++ client/src/components/navbar/navbar.js
@@ -0,0 +1,27 @@
+import { Navbar, Nav, Image, Container } from "react-bootstrap";
+import "./navbar.css";
+import icon from "../../assets/geography.png";
+
+function GQNavbar() {
+    return (
+        <div>
+            <Navbar collapseOnSelect expand="lg" bg="transparent" variant="dark">
+                <Container>
+                    <Nav.Item><Image id="icon" src={icon}></Image></Nav.Item>
+                    <Navbar.Brand id="brand" href="/">GeoQuiz</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse className="justify-content-end" id="responsive-navbar-nav">
+                        <Nav>
+                            <Nav.Link className="link" href="">Dashboard</Nav.Link>
+                            <Nav.Link className="link" >Log In</Nav.Link>
+                            <Nav.Link className="link" >Sign Up</Nav.Link>
+                            <Nav.Link className="link" >Log Out</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    )
+}
+
+export default GQNavbar;

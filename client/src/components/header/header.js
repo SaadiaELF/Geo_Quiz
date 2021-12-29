937ed2fbc74823a987da7aeb689b582fdc397304
SaadiaELF <74792703+SaadiaELF@users.noreply.github.com>
2021-12-29 02:24:10 +0000

    Adding navbar

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -1,8 +1,25 @@
-import { Button } from 'react-bootstrap';
+import { Navbar, Nav, Image, Container } from "react-bootstrap";
+import "./header.css";
+import icon from "../../assets/geography.png";
 
 function Header() {
     return (
         <div>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container >
+                    <Nav.Item><Image id="icon" src={icon}></Image></Nav.Item>
+                    <Navbar.Brand href="/">GeoQuiz</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse className="justify-content-center" id="responsive-navbar-nav">
+                        <Nav>
+                            {/* <Nav.Link href="">Dashboard</Nav.Link>
+                            <Nav.Link href="">Login</Nav.Link>
+                            <Nav.Link href="">SignUp</Nav.Link>
+                            <Nav.Link href="">LogOut</Nav.Link> */}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     )
 }

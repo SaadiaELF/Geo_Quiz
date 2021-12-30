adc6e4b7399b29864b17c1077e7c38d805f88433
SaadiaELF <74792703+SaadiaELF@users.noreply.github.com>
2021-12-30 23:17:00 +0000

    Adding a card

--- client/src/components/hero/hero.js
+++ client/src/components/hero/hero.js
@@ -1,27 +1,20 @@
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Card, Button } from "react-bootstrap";
 import "./hero.css";
 
 function Hero() {
     return (
         <div>
             <Container className="hero">
-                <Row>
-                    <Col md='12' lg="6" xl="5" className="offset-xl-1 text-center mt-5">
-                        <h1>GeoQuiz</h1>
-                        <p>Aute Lorem non occaecat ullamco culpa officia. Adipisicing do do pariatur id. Reprehenderit magna elit eu do laboris pariatur adipisicing dolor eu Lorem sint enim excepteur aliquip. Tempor nostrud esse enim fugiat. Labore reprehenderit dolor cillum ipsum culpa officia nisi amet id dolor veniam dolore incididunt qui. Voluptate consectetur labore ipsum dolor aliquip voluptate sit. </p>
-                        <Button variant="outline-light" size="lg" >
-                            Start
-                        </Button>
-                    </Col>
-                    <Col md="5" lg="5" className="offset-lg-1 offset-xl-0 d-none d-lg-block">
-                    </Col>
-                    {/* <div class="col-12 col-lg-6 col-xl-5 offset-xl-1">
-                        <h1>The revolution is here.</h1>
-                        <p>Mauris egestas tellus non ex condimentum, ac ullamcorper sapien dictum. Nam consequat neque quis sapien viverra convallis. In non tempus lorem. </p><button class="btn btn-light btn-lg action-button" type="button">Learn More</button></div>
-                    <div class="col-md-5 col-lg-5 offset-lg-1 offset-xl-0 d-none d-lg-block phone-holder">
-                        <div class="iphone-mockup"><img src="assets/img/iphone.svg" class="device">
-                            <div class="screen"></div>
-                    </div> */}
+                <Row className="justify-content-center" >
+                    <Card className="homeCard text-center">
+                        <Card.Body>
+                            <h1>GeoQuiz</h1>
+                            <p>Aute Lorem non occaecat ullamco culpa officia. Adipisicing do do pariatur id. Reprehenderit magna elit eu do laboris pariatur adipisicing dolor eu Lorem sint enim excepteur aliquip. Tempor nostrud esse enim fugiat. Labore reprehenderit dolor cillum ipsum culpa officia nisi amet id dolor veniam dolore incididunt qui. Voluptate consectetur labore ipsum dolor aliquip voluptate sit. </p>
+                            <Button variant="outline-light" size="lg" >
+                                Start
+                            </Button>
+                        </Card.Body>
+                    </Card>
                 </Row>
             </Container>
         </div>

22d8ebee9b2e0d232c9e7f9bb6c40418c366f653
SaadiaELF <74792703+SaadiaELF@users.noreply.github.com>
2021-12-30 23:27:48 +0000

    Deletinh header

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/footer/footer";
-import Header from "./components/header/header";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <div>
-        <Header />
         <Routes>
           <Route exact path="/" element={<Home />} />
         </Routes>

e4da545b5a9532ac2b3f7c86aa0b17a4609dfb45
SaadiaELF <74792703+SaadiaELF@users.noreply.github.com>
2021-12-30 23:28:44 +0000

    Adding homepage

---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import GQNavbar from "../components/navbar/navbar";
+import Hero from "../components/hero/hero";
+import "./Home.css";
+
+function Home() {
+    return (
+        <header>
+            <GQNavbar></GQNavbar>
+            <Hero />
+        </header>
+    )
+}
+
+export default Home;

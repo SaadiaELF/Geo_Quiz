e040cdd9d75a56454c98922c406c3b471b4afbf3
SaadiaELF <74792703+SaadiaELF@users.noreply.github.com>
2022-01-01 15:14:24 +0000

    Adding quiz page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/footer/footer";
 import Home from "./pages/Home/Home";
+import Quiz from "./pages/Quiz/Quiz";
 
 function App() {
   return (
@@ -9,8 +10,9 @@ function App() {
       <div>
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/quiz" element={<Quiz />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </div>
     </BrowserRouter>
   );
---
+++ client/src/pages/Quiz/Quiz.js
@@ -0,0 +1,11 @@
+import GQNavbar from "../../components/navbar";
+
+function Quiz() {
+    return (
+        <div>
+            <GQNavbar/>
+        </div>
+    )
+}
+
+export default Quiz;

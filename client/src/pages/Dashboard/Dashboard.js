b9e748470427fbd4fc87f00663abf273d12b9622
SaadiaELF <74792703+SaadiaELF@users.noreply.github.com>
2022-01-01 19:27:03 +0000

    Adding structure

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,3 +1,4 @@
+import { Button, Card, ListGroup } from "react-bootstrap";
 import GQNavbar from "../../components/navbar/navbar";
 import Hero from "../../components/hero/hero";
 import "./Dashboard.css";
@@ -7,7 +8,41 @@ function Home() {
         <header>
             <GQNavbar></GQNavbar>
             <Hero>
-          
+                <Card.Header as="h5" className="text-start">UserName</Card.Header>
+                <Card.Body>
+                    <Card.Title className="text-start">Score History</Card.Title>
+                    <Card.Text className="text-start" >
+                        <ListGroup as="ol" numbered>
+                            <ListGroup.Item as="li" variant="primary" className="d-flex">
+                                <div>
+                                    Score 1
+                                </div>
+                                <div>
+                                    Date
+                                </div>
+                            </ListGroup.Item>
+                            <ListGroup.Item as="li" variant="primary" className="d-flex">
+                                <div>
+                                    Score 2
+                                </div>
+                                <div>
+                                    Date
+                                </div>
+                            </ListGroup.Item>
+                            <ListGroup.Item as="li" variant="primary" className="d-flex">
+                                <div>
+                                    Score 3
+                                </div>
+                                <div>
+                                    Date
+                                </div>
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Card.Text>
+                    <Card.Footer>
+                        <Button variant="danger">Clear All</Button>
+                    </Card.Footer>
+                </Card.Body>
             </Hero>
         </header>
     )

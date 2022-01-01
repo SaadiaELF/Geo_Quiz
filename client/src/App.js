7eda752c88ba70edb4d97587bf3152c3280f0032
SaadiaELF <74792703+SaadiaELF@users.noreply.github.com>
2022-01-01 18:14:22 +0000

    Creating Dashboard page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/footer/footer";
 import Home from "./pages/Home/Home";
 import Quiz from "./pages/Quiz/Quiz";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/quiz" element={<Quiz />} />
+          <Route exact path="/dashboard" element={<Dashboard />} />
         </Routes>
         <Footer />
       </div>
---
+++ client/src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,16 @@
+import GQNavbar from "../../components/navbar/navbar";
+import Hero from "../../components/hero/hero";
+import "./Dashboard.css";
+
+function Home() {
+    return (
+        <header>
+            <GQNavbar></GQNavbar>
+            <Hero>
+          
+            </Hero>
+        </header>
+    )
+}
+
+export default Home;

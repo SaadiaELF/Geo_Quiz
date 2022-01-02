c4585f961f1ccec803e0921a66453c58f51848c8
SaadiaELF <74792703+SaadiaELF@users.noreply.github.com>
2022-01-02 23:48:14 +0000

    Adding click event

--- client/src/pages/Quiz/Quiz.js
+++ client/src/pages/Quiz/Quiz.js
@@ -1,12 +1,11 @@
 import { useState, useEffect } from "react";
-import { Button, Card, ToggleButton } from "react-bootstrap";
+import { Card, ToggleButton } from "react-bootstrap";
 import GQNavbar from "../../components/navbar/navbar";
 import Hero from "../../components/hero/hero";
 import API from "../../utils/API";
 import "./Quiz.css";
 
 function Quiz() {
-    const [radioValue, setRadioValue] = useState();
     const [currentQuestion, setCurrentQuestion] = useState(0);
     const [questions, setQuestions] = useState([{ question: "", correct_answer: "", answers: [] }]);
 
@@ -22,6 +21,15 @@ function Quiz() {
         getQuestions()
     }, []);
 
+    const handleAnswerButtonClick = () => {
+        const nextQuestion = currentQuestion + 1;
+        setCurrentQuestion(nextQuestion);
+        if (nextQuestion < questions.length) {
+            setCurrentQuestion(nextQuestion);
+        } else {
+            alert('you reached the end of the quiz');
+        }
+    };
     return (
         <div>
             <header>
@@ -32,27 +40,23 @@ function Quiz() {
                         <Card.Body>
                             <Card.Title as="h6" className="text-start">{questions[currentQuestion].question}</Card.Title>
                             <Card.Text className="d-grid gap-2">
-                                    {questions[currentQuestion].answers.map((answer,idx) => (
-                                           <ToggleButton
-                                                key={idx}
-                                                id={`radio-${idx}`}
-                                                className="text-start"
-                                                type="radio"
-                                                variant='light'
-                                                name="radio"
-                                                value={answer}
-                                                // checked={radioValue === radio.value}
-                                                // onChange={(e) => setRadioValue(e.currentTarget.value)}
-                                            >
-                                                {answer}
-                                            </ToggleButton>
-                                        ))}
+                                {questions[currentQuestion].answers.map((answer, idx) => (
+                                    <ToggleButton
+                                        key={idx}
+                                        id={`radio-${idx}`}
+                                        className="text-start"
+                                        type="radio"
+                                        variant='light'
+                                        name="radio"
+                                        value={answer}
+                                        onClick={() => handleAnswerButtonClick()}
+                                    >
+                                        {answer}
+                                    </ToggleButton>
+                                ))}
                             </Card.Text>
                         </Card.Body>
-                        <Card.Footer className="text-end">
-                            <Button
-                                variant="danger">Next</Button>
-                        </Card.Footer>
+
                     </Card>
                 </Hero>
             </header>

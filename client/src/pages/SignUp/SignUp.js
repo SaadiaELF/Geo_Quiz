69eddfd9d1b7f88172a59bb24f38c2349f64563a
SaadiaELF <74792703+SaadiaELF@users.noreply.github.com>
2022-01-02 00:22:33 +0000

    Adding form

--- client/src/pages/SignUp/SignUp.js
+++ client/src/pages/SignUp/SignUp.js
@@ -1,4 +1,4 @@
-import { Card } from "react-bootstrap";
+import { Card, Form, Button, Col, Row } from "react-bootstrap";
 import GQNavbar from "../../components/navbar/navbar";
 import Hero from "../../components/hero/hero";
 import "./SignUp.css";
@@ -8,8 +8,49 @@ function SignUp() {
         <header>
             <GQNavbar></GQNavbar>
             <Hero>
-                <Card className="Card text-center">
+                <Card className="signup-card">
                     <Card.Body>
+                        <Form className="text-center">
+                            <Form.Group className="mb-3 text-start" controlId="username">
+                                <Row>
+                                    <Col md="4">
+                                        <Form.Label>Username</Form.Label>
+                                    </Col>
+                                    <Col>
+                                        <Form.Control type="text" placeholder="Username" />
+                                    </Col>
+                                </Row>
+                            </Form.Group>
+                            <Form.Group className="mb-3 text-start" controlId="email">
+                                <Row>
+                                    <Col>
+                                        <Form.Label>Email address</Form.Label>
+                                    </Col>
+                                    <Col>
+                                        <Form.Control type="email" placeholder="Email" />
+                                    </Col>
+                                    <Col>
+                                        <Form.Control type="email" placeholder="Confirm Email" />
+                                    </Col>
+                                </Row>
+                            </Form.Group>
+                            <Form.Group className="mb-3 text-start" controlId="password">
+                                <Row>
+                                    <Col>
+                                        <Form.Label>Password</Form.Label>
+                                    </Col>
+                                    <Col>
+                                        <Form.Control type="password" placeholder="Password" />
+                                    </Col>
+                                    <Col>
+                                        <Form.Control type="password" placeholder="Confirm Password" />
+                                    </Col>
+                                </Row>
+                            </Form.Group>
+                            <Button variant="primary" type="submit">
+                                Submit
+                            </Button>
+                        </Form>
                     </Card.Body>
                 </Card>
             </Hero>

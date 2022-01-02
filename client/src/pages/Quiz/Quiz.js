0067a9c59f51f12078e3da5788a7eea3bf918c16
SaadiaELF <74792703+SaadiaELF@users.noreply.github.com>
2022-01-02 22:20:37 +0000

    Adding questions state

--- client/src/pages/Quiz/Quiz.js
+++ client/src/pages/Quiz/Quiz.js
@@ -1,16 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button, Card, ToggleButton } from "react-bootstrap";
 import GQNavbar from "../../components/navbar/navbar";
 import Hero from "../../components/hero/hero";
+import API from "../../utils/API";
 import "./Quiz.css";
 
 function Quiz() {
-    const [radioValue, setRadioValue] = useState('1');
-    const answers = [
-        { name: 'Answer 1', value: '1' },
-        { name: 'Answer 2', value: '2' },
-        { name: 'Answer 3', value: '3' },
-    ];
+    const [radioValue, setRadioValue] = useState();
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [questions, setQuestions] = useState([{question: "", correct_answer: "", answers: ""}]);
+
+    useEffect(() => {
+        function getQuestions() {
+            API.getQuestions()
+                .then(questions => {
+                    setQuestions(questions);
+                    console.log(questions, "test")
+                })
+                .catch(err => console.log(err));
+        }
+        getQuestions()
+    }, []);
 
     return (
         <div>
@@ -20,28 +30,29 @@ function Quiz() {
                     <Card className="quiz-card text-center">
                         <Card.Header as="h5">Question n°</Card.Header>
                         <Card.Body>
-                            <Card.Title as="h6" className="text-start">Question text</Card.Title>
+                            <Card.Title as="h6" className="text-start">{questions[currentQuestion].question}</Card.Title>
                             <Card.Text className="d-grid gap-2">
-                                {answers.map((radio, idx) => (
-                                    <ToggleButton
-                                        key={idx}
-                                        id={`radio-${idx}`}
-                                        className="text-start"
-                                        type="radio"
-                                        variant='light'
-                                        name="radio"
-                                        value={radio.value}
-                                        checked={radioValue === radio.value}
-                                        onChange={(e) => setRadioValue(e.currentTarget.value)}
-                                    >
-                                        {radio.name}
-                                    </ToggleButton>
-                                ))}
+                                {/* {question.answers.map((answer) => (
+                                            <ToggleButton
+                                                key={idx}
+                                                id={`radio-${idx}`}
+                                                className="text-start"
+                                                type="radio"
+                                                variant='light'
+                                                name="radio"
+                                                value={answer}
+                                                checked={radioValue === radio.value}
+                                                onChange={(e) => setRadioValue(e.currentTarget.value)}
+                                            >
+                                                {radio.name}
+                                            </ToggleButton>
+                                        ))} */}
 
                             </Card.Text>
                         </Card.Body>
                         <Card.Footer className="text-end">
-                            <Button variant="danger">Next</Button>
+                            <Button
+                                variant="danger">Next</Button>
                         </Card.Footer>
                     </Card>
                 </Hero>

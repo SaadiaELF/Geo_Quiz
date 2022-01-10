31cd5245d8cc48c927f0b940cce5f2216b909c90
SaadiaELF <74792703+SaadiaELF@users.noreply.github.com>
2022-01-10 21:44:44 +0000

    Adding  link to button

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,4 +1,4 @@
-import { Button, Card } from "react-bootstrap";
+import { Button, Card, Link } from "react-bootstrap";
 import GQNavbar from "../../components/navbar/navbar";
 import Hero from "../../components/hero/hero";
 import "./Home.css";
@@ -8,15 +8,15 @@ function Home() {
         <header>
             <GQNavbar></GQNavbar>
             <Hero>
-            <Card className="home-card text-center">
-                <Card.Body>
-                    <h1>GeoQuiz</h1>
-                    <p>Aute Lorem non occaecat ullamco culpa officia. Adipisicing do do pariatur id. Reprehenderit magna elit eu do laboris pariatur adipisicing dolor eu Lorem sint enim excepteur aliquip. Tempor nostrud esse enim fugiat. Labore reprehenderit dolor cillum ipsum culpa officia nisi amet id dolor veniam dolore incididunt qui. Voluptate consectetur labore ipsum dolor aliquip voluptate sit. </p>
-                    <Button variant="outline-light" size="lg" >
-                        Start
-                    </Button>
-                </Card.Body>
-            </Card>
+                <Card className="home-card text-center">
+                    <Card.Body>
+                        <h1>GeoQuiz</h1>
+                        <p>Aute Lorem non occaecat ullamco culpa officia. Adipisicing do do pariatur id. Reprehenderit magna elit eu do laboris pariatur adipisicing dolor eu Lorem sint enim excepteur aliquip. Tempor nostrud esse enim fugiat. Labore reprehenderit dolor cillum ipsum culpa officia nisi amet id dolor veniam dolore incididunt qui. Voluptate consectetur labore ipsum dolor aliquip voluptate sit. </p>
+                        <Button href="/quiz" variant="outline-light" size="lg" >
+                            Start
+                        </Button>
+                    </Card.Body>
+                </Card>
             </Hero>
         </header>
     )

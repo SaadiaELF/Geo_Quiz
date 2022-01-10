d5b8cff07d5b692564a6521b14a299e0572c22d5
SaadiaELF <74792703+SaadiaELF@users.noreply.github.com>
2022-01-10 19:08:14 +0000

    Changing answers array

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -8,9 +8,13 @@ export default {
                 const questions = res.data.results;
                 // Map trough out the API results and store the in an array of objects
                 return questions.map(question => {
-                    const answers = question.incorrect_answers;
-                    answers.push(question.correct_answer);
-                    answers.sort(function(a, b){return 0.5 - Math.random()});
+                    const answers = [
+                        [question.incorrect_answers[0], false],
+                        [question.incorrect_answers[1], false],
+                        [question.incorrect_answers[2], false],
+                        [question.correct_answer, true]
+                    ]
+                    answers.sort(function (a, b) { return 0.5 - Math.random() });
                     return {
                         question: question.question,
                         correct_answer: question.correct_answer,

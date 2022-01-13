84571e1be03fd142af60ed5a8c77ba30958ecb59
SaadiaELF <74792703+SaadiaELF@users.noreply.github.com>
2022-01-13 18:26:10 +0000

    Adding userForm component

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { Card, Form, Button, Col, Row } from "react-bootstrap";
 import GQNavbar from "../../components/navbar/navbar";
 import Hero from "../../components/hero/hero";
+import UserForm from "../../components/userForm/userForm";
 import "./Login.css";
 
 function Login() {
@@ -11,30 +12,24 @@ function Login() {
             <Hero>
                 <Card className="login-card">
                     <Card.Header as="h5" className="text-start">Sign in your account</Card.Header>
-
                     <Card.Body>
                         <Form className="text-center">
-
-                            <Form.Group className="mb-3 text-start" controlId="email">
-                                <Row>
-                                    <Col md="4">
-                                        <Form.Label>Email address</Form.Label>
-                                    </Col>
-                                    <Col>
-                                        <Form.Control type="email" placeholder="Email" autoComplete="current-email" />
-                                    </Col>
-                                </Row>
-                            </Form.Group>
-                            <Form.Group className="mb-3 text-start" controlId="password">
-                                <Row>
-                                    <Col md="4">
-                                        <Form.Label>Password</Form.Label>
-                                    </Col>
-                                    <Col>
-                                        <Form.Control type="password" placeholder="Password" autoComplete="current-password" />
-                                    </Col>
-                                </Row>
-                            </Form.Group>
+                            <UserForm
+                                md="4"
+                                col="2"
+                                label="Email address"
+                                type="email"
+                                placeholder="Email"
+                                autocomplete="current-email"
+                            />
+                            <UserForm
+                                md="4"
+                                col="2"
+                                label="Password"
+                                type="password"
+                                placeholder="Password"
+                                autocomplete="current-password"
+                            />
                             <Form.Text className="text-muted float-start">
                                 Don't have an account ? <Link to="/signup">Sign up </Link>
                             </Form.Text>

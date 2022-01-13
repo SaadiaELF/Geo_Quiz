2f27b2e0fb82cb1ae3d76ad34f98588e988dbbbc
SaadiaELF <74792703+SaadiaELF@users.noreply.github.com>
2022-01-13 16:46:31 +0000

    Hashing password using bcryptjs

--- models/user.js
+++ models/user.js
@@ -1,24 +1,48 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs")
 
 const userSchema = new mongoose.Schema({
     id: mongoose.ObjectId,
     username: {
-        type: String, 
+        type: String,
         required: true,
         unique: true
     },
     email: {
-        type: String, 
+        type: String,
         required: true,
         unique: true,
         match: [/.+\@.+\..+/, 'Please fill a valid email address']
     },
     password: {
-        type: String, 
+        type: String,
         required: true,
         minlength: [8, 'Password must be at least 8 characters']
     },
     image: String,
+});
+
+userSchema.pre("save", function (next) {
+    const user = this
+
+    if (this.isModified("password") || this.isNew) {
+        bcrypt.genSalt(10, function (saltError, salt) {
+            if (saltError) {
+                return next(saltError)
+            } else {
+                bcrypt.hash(user.password, salt, function (hashError, hash) {
+                    if (hashError) {
+                        return next(hashError)
+                    }
+
+                    user.password = hash
+                    next()
+                })
+            }
+        })
+    } else {
+        return next()
+    }
 });
 
 const User = mongoose.model("User", userSchema);

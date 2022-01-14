903121acdf6a5903e386be43febab54b8b590f7f
SaadiaELF <74792703+SaadiaELF@users.noreply.github.com>
2022-01-14 18:20:41 +0000

    Fixing post route issue

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,14 +1,4 @@
-// const db = require("../models");
-
-// module.exports = {
-//     // Save user to database
-//     create: function (req, res) {
-//         db.User
-//             .create(req.body)
-//             .then(dbModel => res.json(dbModel))
-//             .catch(err => res.status(422).json(err));
-//     },
-// };
+// to modify later
 exports.allAccess = (req, res) => {
     res.status(200).send("Public Content.");
 };
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -11,11 +11,7 @@ module.exports = function (app) {
     });
 
     app.post(
-        "/api/auth/signup",
-        [
-            verifySignUp.checkDuplicateUsernameOrEmail,
-        ],
-        controller.signup
+        "/api/auth/signup", verifySignUp.checkDuplicateUsernameOrEmail, controller.signup
     );
 
     app.post("/api/auth/signin", controller.signin);

467e1f643d4d81d9151d3d84d327e9fc888c709c
SaadiaELF <74792703+SaadiaELF@users.noreply.github.com>
2022-01-14 00:49:35 +0000

    Open Mongoose connection to MongoDB database

--- server.js
+++ server.js
@@ -15,6 +15,23 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
+const db = require("./app/models");
+
+db.mongoose
+  .connect(process.env.MONGODB_URI || `mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch(err => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+
+  
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to geoquiz application." });
@@ -23,12 +40,3 @@ app.get("/", (req, res) => {
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, console.log(`Server is running on port ${PORT}.`))
-
-
-// const mongoose = require('mongoose');
-
-
-// // Mongo DB connection
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/geoquiz")
-//     .then(() => console.log('DB connected'))
-//     .catch(err => console.log(err));

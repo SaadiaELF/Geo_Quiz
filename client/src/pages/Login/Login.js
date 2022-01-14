8b4a8503007ffc91bb084651f954d0da061469d5
SaadiaELF <74792703+SaadiaELF@users.noreply.github.com>
2022-01-14 23:33:37 +0000

    Adding handleLogin function

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,18 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Card, Form, Button } from "react-bootstrap";
 import GQNavbar from "../../components/navbar/navbar";
 import Hero from "../../components/hero/hero";
 import UserForm from "../../components/userForm/userForm";
-import AuthService from "../../services/auth.service"
+import AuthService from "../../services/auth.service";
 import "./Login.css";
 import { useState } from "react";
 
 function Login() {
+    const navigate = useNavigate();
     const [user, setUser] = useState({
         email: "",
         password: "",
-        loading: false,
-        message: ""
+        // message: ""
     });
     const onChangeEmail = (e) => {
         setUser({ ...user, email: e.target.value })
@@ -21,15 +21,18 @@ function Login() {
     const onChangePassword = (e) => {
         setUser({ ...user, password: e.target.value })
     }
-    const required = value => {
-        if (!value) {
-            return (
-                <div className="alert alert-danger" role="alert">
-                    This field is required!
-                </div>
-            );
-        }
-    };
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        AuthService.login(user.email, user.password).then(
+            () => {
+                navigate("/dashboard");
+                window.location.reload();
+            },
+            // error => { console.log(error) }
+
+        );
+    }
 
     return (
         <header>
@@ -38,7 +41,7 @@ function Login() {
                 <Card className="login-card">
                     <Card.Header as="h5" className="text-start">Sign in your account</Card.Header>
                     <Card.Body>
-                        <Form className="text-center">
+                        <Form className="text-center" onSubmit={handleLogin}>
                             <UserForm
                                 md="4"
                                 col="2"
